f5a1bde9911e348d03a76f335c1e45bb613cd920
JeerTeles <jadersoncontatos@gmail.com>
2025-06-03 02:12:45 -0300

    feature/ app globalPipes()  ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 
   const config = new DocumentBuilder()
